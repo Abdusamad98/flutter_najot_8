2518536e5fbb10ef29bef0521573e86131babacb
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-05-25 20:31:56 +0500

    lesson 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,121 +5,205 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
+      themeMode: ThemeMode.dark,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    debugPrint("SCREEN HEIGHT: $screenHeight and SCREEN WIDTH: $screenWidth");
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: const Color(0xFFE5E5E5),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color(0xFFE5E5E5),
+          title: const Text(
+            "Profile Screen",
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            onPressed: () {},
+          ),
+          // actions: [
+          //   IconButton(
+          //     icon: const Icon(Icons.more_vert),
+          //     onPressed: () {
+          //     },
+          //   ),
+          //
+          // ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Column(
+          children: [
+            const SizedBox(height: 42),
+            const Icon(
+              Icons.person_pin,
+              size: 72,
+              color: Colors.red,
+            ),
+            const SizedBox(height: 21),
+            const Text(
+              "Itunuoluwa Abidoye",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: Color(0xFF181D27),
+              ),
+            ),
+            const Text(
+              "Itunuoluwa@petra.africa",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFFABABAB),
+              ),
+            ),
+            const SizedBox(height: 40),
+            _getMyContainer(title: "What’s your first name?"),
+            _myDivider(),
+            const SizedBox(height: 20),
+            _getMyContainer(title: "And your last name?"),
+            _myDivider(),
+            const SizedBox(height: 20),
+            _getMyContainer(title: "Phone number", type: 2),
+            _myDivider(),
+            const SizedBox(height: 20),
+            _getMyContainer(
+              title: "Phone number",
+              type: 3,
+              iconData: Icons.expand_more,
+            ),
+            _myDivider(),
+            const SizedBox(height: 20),
+            _getMyContainer(
+                title: "Phone number",
+                type: 3,
+                iconData: Icons.calendar_month_rounded),
+            _myDivider(),
+            const SizedBox(height: 34),
+            ElevatedButton(onPressed: () {}, child: const Text("Update Profile"))
+          ],
+        )
+        // Container(
+        //   height: 300,
+        //   width: 300,
+        //   padding: EdgeInsets.all(16),
+        //   margin: EdgeInsets.all(16),
+        //   decoration: BoxDecoration(
+        //     color: Colors.red,
+        //     borderRadius: BorderRadius.all(Radius.circular(16)),
+        //   ),
+        //   child: Center(
+        //     child: Text(
+        //       "Itunuoluwa Abidoye",
+        //       textAlign: TextAlign.center,
+        //       style: TextStyle(
+        //           fontSize: 16,
+        //           fontWeight: FontWeight.w700,
+        //           color: Color(0xFF181D27),
+        //           shadows: []),
+        //     ),
+        //   ),
+        // ),
+        );
   }
+
+  // 1 bolsa oddiy text
+  // 2 bolsa bayroqli text
+  // 3 bols iconli text
+
+  Widget _getMyContainer({
+    required String title,
+    int type = 1,
+    IconData? iconData,
+  }) {
+    return Container(
+        width: double.infinity,
+        height: 54,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 18,
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(16),
+            topLeft: Radius.circular(16),
+          ),
+          // boxShadow: [
+          //   BoxShadow(
+          //     blurRadius: 12,
+          //     spreadRadius: 10,
+          //     offset: const Offset(0, 0),
+          //     color: Colors.grey.shade300,
+          //   ),
+          // ],
+        ),
+        child: Row(
+          mainAxisAlignment: type == 3
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.start,
+          children: [
+            if (type == 2)
+              const Icon(
+                Icons.flag_rounded,
+                color: Colors.green,
+              ),
+            if (type == 2) const VerticalDivider(),
+            Text(
+              title,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF555555),
+              ),
+            ),
+            if (type == 3) Icon(iconData),
+          ],
+        ));
+  }
+
+  Widget _myDivider() => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        height: 1,
+        color: Colors.grey,
+      );
 }
+
+// MaterialApp
+// Container
+// Icon
+// AppBar
+// SizedBox
+// Text
+// Column
+// Row
+// Divider
+// VerticalDivider
+// EdgeInsets
+// BoxDecoration
+

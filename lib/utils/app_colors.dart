b3d5296fde026f717ed3397722e6cd42e811461f
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-05-26 19:22:17 +0500

    lesson utils , set fonts, set images,

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color appMainColor = Color(0xFF10405A);
+  static const Color C_D5E7D4 = Color(0xFFD5E7D4);
+  static const Color C_FFCA42 = Color(0xFFFFCA42);
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,3 @@
+class AppImages{
+  static const String bicycle = "assets/images/bicycle.png";
+}
